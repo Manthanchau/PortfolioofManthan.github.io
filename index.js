0cfb75d35c08cf179c210581e0c62427c2f9530d
ManthanChaudhari <121889086+Manthanchau@users.noreply.github.com>
2023-03-13 20:10:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("li")[0].addEventListener("click" ,()=>{
+  // document.querySelector(".nav").style.background= "#FF5D5D";
+  document.querySelector("a").style.color= "#FF5D5D";
+  document.getElementsByTagName("svg")[0].style.background = "black";
+  document.querySelector(".navbar").classList.add("navbarutil");
+});
